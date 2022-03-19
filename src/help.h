a04a0bd1c0c1835af7cc095e1b752ed357178dac
Ayush <xcplusplus9@gmail.com>
2022-03-19 13:42:58 +0530

    Minor corrections in help.h

--- src/help.h
+++ src/help.h
@@ -4,7 +4,7 @@
 
 #define INTRO_TEXT "MicroVM version 0.1\nCopyright (c) 2022 Ayush Sharma.\n"
 
-#define H_TEXT_1 INTRO_TEXT  "A small VM originally based on the P'' formal language.\n\n"
+#define H_TEXT_1 INTRO_TEXT  "A small VM loosely based on the P'' formal language.\n\n"
 #define H_TEXT_2 "Usage MICROVM [-c | -d] [-o] [-h | -help] file.\n\n"
 #define H_TEXT_3 "-h | -help      Shows this message.\n"
 #define H_TEXT_4 "-c file         Compiles the given MVScript file.\n"
